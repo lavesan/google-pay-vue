e6db2987be1097faa02faa14c820132b3caab474
valdery <valdery.jur@gmail.com>
2020-11-18 13:41:55 -0300

    feat[checkbox]: Adds component to be used when some checkbox is needed
    Signed-off-by: valdery <valdery.jur@gmail.com>

---
+++ src/components/form/Checkbox.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="form-checkbox">
+        <input
+            type="checkbox"
+            :checked="checked"
+            :id="id"
+            @change="onChange"
+            :name="name" />
+        <label
+            :for="id"
+            :class="`form-checkbox--label ${checked && 'form-checkbox--label form-checkbox--checked'}`">
+            {{ label }}
+        </label>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'CheckboxComponent',
+    props: [
+        'label',
+        'checked',
+        'name',
+        'id',
+        'onChange',
+    ],
+}
+</script>
+
+<style scoped>
+.form-checkbox {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+}
+
+.form-checkbox .form-checkbox--checked {
+    text-decoration: line-through;
+}
+
+.form-checkbox .form-checkbox--label {
+    margin-left: 8px;
+}
+</style>
