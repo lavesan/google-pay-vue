d449349efb38b96eefb4de25f8c2aac43f48129b
“Valdery <valdery.jur@gmail.com>
2021-08-24 13:07:44 -0300

    Removes unused logic
    Signed-off-by: “Valdery <valdery.jur@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld /> -->
     <h1>
-      Botão google pay:
+      Botão google pay
     </h1>
     <GooglePay totalPrice="100.00" />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './views/HelloWorld'
 import GooglePay from './components/GooglePay.vue';
 
 export default {
   name: 'App',
-  mounted() {
-    // const recaptchaScript = document.createElement("script");
-    // recaptchaScript.setAttribute("src", "https://pay.google.com/gp/p/js/pay.js");
-    // recaptchaScript.async = true;
-    // recaptchaScript.onload = this.$refs.googlePayButton.onGooglePayLoaded;
-    // document.head.appendChild(recaptchaScript);
-  },
   components: {
-    // HelloWorld,
     GooglePay,
   }
 }
